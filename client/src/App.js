d10477a22cdf3f336d6b35624fa67057acb671ed
thesoundfromthesky <nagara@naver.com>
2019-11-11 16:58:24 -0600

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
-import SavedList from './Movies/SavedList';
+import "./stylesheet.css";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
+    <BrowserRouter>
+      <div>
+        <SavedList list={savedList} />
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" render={props => <Movie {...props} />} />
+      </div>
+    </BrowserRouter>
   );
 };
 
